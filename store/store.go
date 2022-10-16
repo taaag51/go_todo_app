6cab3c082f1d5e318788715c506a518fe18b3302
taaag51 <inat0510+git@gmail.com>
2022-10-16 17:44:00 +0900

    entity.Task仮実装

---
+++ store/store.go
@@ -0,0 +1,36 @@
+package store
+
+import (
+	"errors"
+
+	"github.com/taaag51/go_todo_app/entitiy"
+)
+
+var (
+	Tasks = &TaskStore{Tasks: map[int]*entity.Task{}}
+
+	ErrNotFound = errors.New("not found")
+)
+
+type TaskStore struct {
+	// 動作確認用の仮実装なのであえてexportしてる
+	LastID entitiy.TaskID
+	Tasks  map[entitiy.TaskID]*entitiy.Task
+}
+
+func (ts *TaskStore) Add(t *entitiy.Task) (int, error) {
+	ts.LastID++
+	t.ID = ts.LastID
+	ts.Tasks[t.ID] = t
+	return t.ID, nil
+
+}
+
+// All はソート済みタスク一覧を返す
+func (ts *TaskStore) All() entitiy.Tasks {
+	tasks := make([]*entitiy.Task, len(ts.Tasks))
+	for i, t := range ts.Tasks {
+		tasks[i-1] = t
+	}
+	return tasks
+}
