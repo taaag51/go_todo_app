58ed05420a1343a42405cb5d4f5bf318947bc085
taaag51 <inat0510+git@gmail.com>
2022-10-16 13:32:30 +0900

    テストコードをserver向けに書き換えるまで

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/taaag51/go_todo_app/config"
 	"golang.org/x/sync/errgroup"
@@ -20,6 +23,8 @@ func main() {
 }
 
 func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -33,6 +38,8 @@ func run(ctx context.Context) error {
 	s := &http.Server{
 		// 引数で受け取ったnet.Listenerを利用するのでAddrフィールドは指定しない
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// コマンドラインで実験するため
+			time.Sleep(5 * time.Second)
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
