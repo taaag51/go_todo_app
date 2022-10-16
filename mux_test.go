ba13e98a6c446c5e8a626164878fffc83140f4ab
taaag51 <inat0510+git@gmail.com>
2022-10-16 17:38:00 +0900

    fix mux test

--- mux_test.go
+++ mux_test.go
@@ -23,7 +23,7 @@ func TestNewMux(t *testing.T) {
 		t.Fatalf("faild to read body: %v", err)
 	}
 
-	want := `{"status": "ok"}`
+	want := `{"status":"ok"}`
 	if string(got) != want {
 		t.Errorf("want %q, but got %q", want, got)
 	}
